7985103db457ba86a9b216c83bf740359898fa7f
Muhammad Shahzad <mqasims@live.com>
2021-12-12 20:06:40 -0500

    updated

---
+++ build/precache-manifest.923ae0e2c6a733d06adb06b164d561b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb34bd306689f4a02c7d16b7f1603953",
+    "url": "/GitHub-Education-Portfolio/index.html"
+  },
+  {
+    "revision": "73af00cb2d0e81085ba1",
+    "url": "/GitHub-Education-Portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "afe83c007f0b24c7abaa",
+    "url": "/GitHub-Education-Portfolio/static/js/2.b0986d47.chunk.js"
+  },
+  {
+    "revision": "73af00cb2d0e81085ba1",
+    "url": "/GitHub-Education-Portfolio/static/js/main.cada1b4d.chunk.js"
+  },
+  {
+    "revision": "5e7d53efc42e27e2deba",
+    "url": "/GitHub-Education-Portfolio/static/js/runtime~main.60ee6b4b.js"
+  }
+]);
